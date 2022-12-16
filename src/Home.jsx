ff526d07b7d5ddf578aee46254bc7973dedb0a1e
IMNOTXS <xs90035190@gmail.com>
2022-12-17 01:39:12 +0300

    files

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="container">
+      <div className="row">
+        <div className="user-list">
+          <div className="user">
+            <div className="user-card">
+              <div className="user-card__container">
+                <h3>{user.name}</h3>
+                <p>
+                  <b>Email:</b> {user.email}
+                </p>
+                <p>
+                  <b>Phone:</b> {user.phone}
+                </p>
+                <p>
+                  <b>Website:</b>
+                  {user.website}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,25 +1,69 @@
-import React, {useEffect} from 'react'
-import {useParams} from 'react-router-dom'
-import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import {  useParams, useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
-const Posts = ()=> {
-  const {id}= useParams();
+const Posts = () => {
+  let navigate = useNavigate()
+  const { id } = useParams()
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [searchId,setSearchID] = useState(id)
 
+  function onSearch(){
+    fetchPosts(searchId)
+  }
 
-    useEffect(() => { 
-    async function fetchPosts(){
-      const data = await fetch ("https://jsonplaceholder.typicode.com/posts/1")
-        console.log(data)
-        const data2 = await data.json()
-        console.log(data2)
+  async function fetchPosts(userId) {
+    setLoading(true)
+    const { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts?userId=${userId || id}`,
+    )
+    setPosts(data)
+    setLoading(false)
+  }
 
-    }
-    fetchPosts();
+  useEffect(() => {
+    fetchPosts()
   }, [])
+
+
+  function onSearchKeyPress(key){
+    if (key === 'Enter'){
+      onSearch()
+    }
+  }
+  
   return (
-    <div>
-      {id}
+    <>
+      <div>
+        <div className="post__search">
+          <button onClick={() => navigate('/')}>← Back</button>
+          <div className="post__search--container">
+            <label className="post__search--label">Search by Id</label>
+            <input type="number" value={searchId} onKeyPress={(event) => onSearchKeyPress(event.key)} onChange={(event) => setSearchID(event.target.value)}/>
+            <button onClick={() => onSearch()}>Enter</button>
+          </div>
+        </div>
+        {loading ?
+          new Array(10).fill(0).map((_, index) => (
+            <div className="post">
+              <div className="post__title">
+                <div className="post__title--skeleton"></div>
+              </div>
+              <div className="post__body">
+                <p className="post__body--skeleton"></p>
+              </div>
+            </div>
+          )) : (
+            posts.map((post) => (
+              <div className="post" key={post.id}>
+                <div className="post__title">{post.title}</div>
+                <p className="post__body">{post.body}</p>
+              </div>
+            ))
+          )}
       </div>
+    </>
   )
 }
 
