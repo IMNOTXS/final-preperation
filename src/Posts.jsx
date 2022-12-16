e87b4786f8237cecbc8d848ba90e4a810fd28953
IMNOTXS <xs90035190@gmail.com>
2022-12-17 01:10:40 +0300

    files

---
+++ src/Posts.jsx
@@ -0,0 +1,26 @@
+import React, {useEffect} from 'react'
+import {useParams} from 'react-router-dom'
+import axios from 'axios';
+
+const Posts = ()=> {
+  const {id}= useParams();
+
+
+    useEffect(() => { 
+    async function fetchPosts(){
+      const data = await fetch ("https://jsonplaceholder.typicode.com/posts/1")
+        console.log(data)
+        const data2 = await data.json()
+        console.log(data2)
+
+    }
+    fetchPosts();
+  }, [])
+  return (
+    <div>
+      {id}
+      </div>
+  )
+}
+
+export default Posts
